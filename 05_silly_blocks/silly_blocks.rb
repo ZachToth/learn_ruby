7141c3bbcc8dd12b1e22222803fb2efc67ca3f3a
Zachary Toth <zachtoth@gmail.com>
2015-02-03 14:38:04 -0500

    Completed silly_blocks.rb

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,1 +1,23 @@
-silly_blocks.rb
+def reverser
+  array = yield.split(" ")
+  array.each { |x| x.reverse! }
+  array.join(" ")
+end
+
+# def adder
+#   num = yield
+#   num + 1
+# end
+
+def adder(multi = 1)
+  num = yield
+  num + multi
+end
+
+def repeater(time = 1)
+ while time > 0
+ num = yield
+ time -= 1
+end
+
+end
